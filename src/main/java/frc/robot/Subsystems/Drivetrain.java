25b6e63e5c5559e589f9f09be08b2ff3bcf34f0d
Bronx High School of Science <School@Robotics-02.bxsci.78x445.nycboe.org>
2022-01-15 15:54:54 -0500

    initial commit

---
+++ src/main/java/frc/robot/Subsystems/Drivetrain.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/** Add your docs here. */
+public class Drivetrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
