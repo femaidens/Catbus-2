dcbfb03665712d9f0624a48945dd178eddbdc49a
LudtheDude <pajol@bxscience.edu>
2022-02-18 16:14:24 -0500

    added command for align

---
+++ src/main/java/frc/robot/Commands/AlignShooter.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class AlignShooter extends Command {
+  public AlignShooter() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.shooter);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {}
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.shooter.alignShooter();
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {}
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {}
+}
