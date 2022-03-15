fd1e4782148186a3e677616b1fc190d9b2e50fa8
Jillian Chong <chongj@bxscience.edu>
2022-03-15 17:50:41 -0400

    fixed errors

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -6,15 +6,15 @@ package frc.robot;
 
 /** Add your docs here. */
 public class RobotMap {
-    public static int driveJoy1Port = 1; //controls lateral
-    public static int driveJoy2Port = 0; //controls turning
+    public static int driveJoy1Port = 0; //controls lateral
+    public static int driveJoy2Port = 1; //controls turning
 
     //drivetrain - might need to fix
-    public static int frontRightPort = 9;
-    public static int frontLeftPort = 10;
-    public static int rearRightPort = 3;
-    public static int rearLeftPort = 11;
-    public static int gyroPort = 0;
+    public static int frontRightPort = 11; //10
+    public static int frontLeftPort = 3; //9
+    public static int rearRightPort = 9; //11
+    public static int rearLeftPort = 10; //3
+    public static int gyroPort = 0; //0
 
 
 }
